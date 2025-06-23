04ef314020c40d2e4febfe6fd5cbd594146c7ad5
Karim Shaaban <karimatef785@gmail.com>
2025-06-23 17:56:58 +0300

    express without much ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+const app = express();
+app.use(cookieSession({keys:['justARandomKeyForNow']}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
